ea800e301d5cb01d8aa9bb55e974241916779818
Dmitry Novik <dmitry.novik@gmail.com>
2019-01-21 14:45:13 +1100

    Add project files.

---
+++ MassTransitPlayground/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MassTransitPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
