0093d4e3b1ad03d08b1cafd460c1c602f7bc6016
Dmitry Novik <dmitry.novik@gmail.com>
2019-01-21 14:57:38 +1100

    Init

---
+++ MassTransitPlayground/MassTransitTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MassTransit;
+using Xunit;
+
+namespace MicroService.PassengerReconciliation.Tests
+{
+    public class Message
+    {
+        public string Payload { get; set; }
+        public override string ToString() => Payload;
+    }
+
+    public class MassTransitTest
+    {
+        private readonly EventWaitHandle _event = new ManualResetEvent(false);
+
+        [Fact]
+        public async Task MassTransit_InMemory_Test()
+        {
+            const string expectedMessage = "Testing MassTransit in-memory bus";
+
+            var busControl = Bus.Factory.CreateUsingInMemory(cfg =>
+            {
+                cfg.ReceiveEndpoint("queue_name", ep =>
+                {
+                    //configure the endpoint
+                    ep.Handler<Message>(m =>
+                    {
+                        Console.WriteLine(m.Message);
+                        _event.Set();
+                        m.Message.Should().Be(expectedMessage);
+                        return Task.CompletedTask;
+                    });
+                });
+            });
+
+            await busControl.StartAsync();
+            await busControl.Publish(new Message { Payload = expectedMessage });
+            _event.WaitOne();
+            await busControl.StopAsync();
+        }
+    }
+}
--- MassTransitPlayground/Program.cs
+++ MassTransitPlayground/Program.cs
@@ -2,11 +2,12 @@
 
 namespace MassTransitPlayground
 {
+    /*
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
-    }
+    }*/
 }
